51ccd3d8437e26869a143bf8bbce5a319ee7ace7
JoseTomasSilvaZ <josetomassilvaz@gmail.com>
2024-02-24 02:46:39 -0300

    Add Flow client and remove unused code

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import FlowPaymentClient2 from './payment-client'
-// 1FAF41B6-10BA-40CF-9438-4A5DL1BE01BE
-async function example () {
-  newInstance.generatePaymentOrder({ amount: 32000, commerceOrder: '123', email: 'josetomassilvaz@gmail.com', subject: 'xd', timeout: 10, urlConfirmation: 'http://localhost:3000/api/v1/flow/transaction-callback', urlReturn: 'http://localhost:3000/api/v1/flow/transaction-callback' })
-}
-
-example() // Call the async function
