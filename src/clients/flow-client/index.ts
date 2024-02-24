bcf82313096623762105589436f03cc36c39efac
JoseTomasSilvaZ <josetomassilvaz@gmail.com>
2024-02-24 05:35:09 -0300

    Add FlowPlansClient to Flow class

--- src/clients/flow-client/index.ts
+++ src/clients/flow-client/index.ts
@@ -1,13 +1,16 @@
 import { type Environments } from '../base-client/base'
 import FlowPaymentClient from '../payment-client'
+import FlowPlansClient from '../plans-client'
 import FlowRefundClient from '../refund-client'
 
 export class Flow {
   payments: FlowPaymentClient
   refunds: FlowRefundClient
+  plans: FlowPlansClient
 
   constructor (apiKey: string, env: Environments, secret: string) {
     this.payments = new FlowPaymentClient(apiKey, env, secret)
     this.refunds = new FlowRefundClient(apiKey, env, secret)
+    this.plans = new FlowPlansClient(apiKey, env, secret)
   }
 }
