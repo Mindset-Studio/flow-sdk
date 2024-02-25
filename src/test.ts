171e09f194a8a0c4de55be5620ed9c36279e5fc2
JoseTomasSilvaZ <josetomassilvaz@gmail.com>
2024-02-25 04:02:00 -0300

    Remove unused code in test.ts

--- src/test.ts
+++
@@ -1,5 +0,0 @@
-import { Flow } from './clients/flow-client'
-
-const flow = new Flow('1FAF41B6-10BA-40CF-9438-4A5DL1BE01BE', 'development', 'e60a35ceb3c6b814f1e08e039157b92ce08df9cf')
-// const order = flow.payments.getPaymentOrderStatus('C79F6986C5D9374BFC8BAC96ECDE5F056B75EA3C').then(data => { console.log(data) }).catch(error => { console.log(error) })
-flow.refunds.generateRefund({ amount: 32000, commerceTrxId: '123456', flowTrxId: 2062790, receiverEmail: 'josetomassilvaz@gmail.com', urlCallBack: 'https://www.google.com', refundCommerceOrder: '12346' }).then(data => { console.log(data) }).catch(error => { console.log(error) })
