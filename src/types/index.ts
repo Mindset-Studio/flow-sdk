2f8e6d666fcaa5183c84fb455e2580ea433a9933
JoseTomasSilvaZ <josetomassilvaz@gmail.com>
2024-02-25 19:08:44 -0300

    Refactor ListResponse interface in types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,14 @@
-
+// import * from '../clients/coupon-client/types'
 
 export interface ListResponse<T> {
-    total: number
-    hasMore:number
-    data: T[]
+  total: number
+  hasMore: number
+  data: T[]
 }
 
 export interface Filter {
-    start?: number
-    limit?: number
-    filter?: string
-    status?: number
+  start?: number
+  limit?: number
+  filter?: string
+  status?: number
 }
