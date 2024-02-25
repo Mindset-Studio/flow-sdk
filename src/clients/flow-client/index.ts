ee06b0564f98e4c8f3c509d0cd3a69f482e8d7e5
JoseTomasSilvaZ <josetomassilvaz@gmail.com>
2024-02-24 22:45:56 -0300

    Add new client classes to Flow class

--- src/clients/flow-client/index.ts
+++ src/clients/flow-client/index.ts
@@ -1,4 +1,7 @@
 import { type Environments } from '../base-client/base'
+import FlowCouponClient from '../coupon-client'
+import FlowInvoiceClient from '../invoice-client'
+import FlowMerchantClient from '../merchant-client'
 import FlowPaymentClient from '../payment-client'
 import FlowPlansClient from '../plans-client'
 import FlowRefundClient from '../refund-client'
@@ -7,10 +10,16 @@ export class Flow {
   payments: FlowPaymentClient
   refunds: FlowRefundClient
   plans: FlowPlansClient
+  merchants: FlowMerchantClient
+  coupons: FlowCouponClient
+  invoices: FlowInvoiceClient
 
   constructor (apiKey: string, env: Environments, secret: string) {
     this.payments = new FlowPaymentClient(apiKey, env, secret)
     this.refunds = new FlowRefundClient(apiKey, env, secret)
     this.plans = new FlowPlansClient(apiKey, env, secret)
+    this.merchants = new FlowMerchantClient(apiKey, env, secret)
+    this.coupons = new FlowCouponClient(apiKey, env, secret)
+    this.invoices = new FlowInvoiceClient(apiKey, env, secret)
   }
 }
