de54be3b871433522f10a716fa2ee2a56514f782
JoseTomasSilvaZ <josetomassilvaz@gmail.com>
2024-02-24 21:46:00 -0300

    Commented out unused code in index.ts

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,9 +1,9 @@
-import { Flow } from '../clients/flow-client'
+// import { Flow } from '../clients/flow-client'
 
-const flowClient = new Flow('1FAF41B6-10BA-40CF-9438-4A5DL1BE01BE', 'development', 'e60a35ceb3c6b814f1e08e039157b92ce08df9cf')
+// const flowClient = new Flow('1FAF41B6-10BA-40CF-9438-4A5DL1BE01BE', 'development', 'e60a35ceb3c6b814f1e08e039157b92ce08df9cf')
 
-flowClient.payments.generatePaymentOrder({ commerceOrder: `${Math.floor(Math.random() * 100000)}`, amount: 13333, email: 'josetomassilvaz@gmail.com', subject: 'test', timeout: 10, urlConfirmation: 'https://www.google.com', urlReturn: 'https://www.google.com' })
-  .then(data => {
-    console.log(data)
-    flowClient.payments.getPaymentOrderStatus(data.raw.token).then(data => { console.log(data) }).catch(error => { console.log(error) })
-  }).catch(error => { console.log(error) })
+// flowClient.payments.generatePaymentOrder({ commerceOrder: `${Math.floor(Math.random() * 100000)}`, amount: 13333, email: 'josetomassilvaz@gmail.com', subject: 'test', timeout: 10, urlConfirmation: 'https://www.google.com', urlReturn: 'https://www.google.com' })
+//   .then(data => {
+//     console.log(data)
+//     flowClient.payments.getPaymentOrderStatus(data.raw.token).then(data => { console.log(data) }).catch(error => { console.log(error) })
+//   }).catch(error => { console.log(error) })
