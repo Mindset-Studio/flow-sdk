2f0c8118c365f88bb0487831bb2efba89abbcad3
JoseTomasSilvaZ <josetomassilvaz@gmail.com>
2024-02-25 02:14:49 -0300

    Add ListResponse and Filter interfaces to types

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+
+
+export interface ListResponse<T> {
+    total: number
+    hasMore:number
+    data: T[]
+}
+
+export interface Filter {
+    start?: number
+    limit?: number
+    filter?: string
+    status?: number
+}
