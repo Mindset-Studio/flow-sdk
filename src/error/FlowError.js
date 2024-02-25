ea1e11b8b02b2d016e3d343c2b8aee320c8c2e31
JoseTomasSilvaZ <josetomassilvaz@gmail.com>
2024-02-25 04:01:43 -0300

    Delete FlowError class

--- src/error/FlowError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FlowError = void 0;
-class FlowError extends Error {
-    constructor(message, title) {
-        super(`${title}: \n ${message}`);
-    }
-}
-exports.FlowError = FlowError;
